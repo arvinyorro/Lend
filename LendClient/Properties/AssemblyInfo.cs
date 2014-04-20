043f96e9104b399fe4d8e17fddab0587ba437f30
arvinyorro <yorro.a@gmail.com>
2014-04-20 15:48:50 +0800

    Added homepage, changed LendClient project name and namespace to Lend.Wpf.

--- LendClient/Properties/AssemblyInfo.cs
+++ LendClient/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LendClient")]
+[assembly: AssemblyTitle("Lend.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LendClient")]
+[assembly: AssemblyProduct("Lend.Wpf")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
